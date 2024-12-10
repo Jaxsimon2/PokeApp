0f34957f586937e7425b9ae3b92f21fead9ce8cc
JRustulka <jrustulka@gmail.com>
2024-12-10 15:53:23 -0700

    Update Commit

--- app/page.js
+++ app/page.js
@@ -1,22 +1,33 @@
 "use client"
 import Link from "next/link";
 import Image from "next/image";
-
-const handleSearch = () => {
-
-
-}
+import searchPokemon from "./search";
+import {useState} from "react";
 
 export default function Page() {
+
+  const [searchWord, setSearchWord] = useState("Ledian");
+
+  const submitSearch = (event) => {
+    event.preventDefault();
+    searchPokemon(searchWord);
+  }
+  
+  const handleSearch = (event) => {
+    let searchWord = event.target.value;
+    setSearchWord(searchWord);
+  }
+
   return (
     <main>
       <h1>PokeApp</h1>
-      <form onSubmit={(event => handleSearch(event))}>
+      <form onSubmit={(event => submitSearch(event))}>
         <input
           required
           placeholder="Enter Pokemon Name Here"
           id="name"
-          type="text"/>
+          type="text"
+          onChange={(event) => handleSearch(event)}/>
         <input type="submit" value="Submit"/>
       </form>
       
