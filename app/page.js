1731de39467dc001522efb460092df1ef6f5d156
JRustulka <jrustulka@gmail.com>
2024-12-10 18:50:02 -0700

    Last Commit

--- app/page.js
+++ app/page.js
@@ -1,11 +1,12 @@
 "use client"
-import Link from "next/link";
+
 import Image from "next/image";
 import searchPokemon from "./search";
 import {useState} from "react";
 
 export default function Page() {
 
+<<<<<<< Updated upstream
   const [searchWord, setSearchWord] = useState("Ledian");
 
   const submitSearch = (event) => {
@@ -23,14 +24,36 @@ export default function Page() {
       <h1>PokeApp</h1>
       <form onSubmit={(event => submitSearch(event))}>
         <input
+=======
+  const [searchPhrase, setSearchPhrase] = useState("Ledian");
+
+  const submitSearch = (event) => {
+    event.preventDefault();
+  }
+  
+  const handleSearch = (event) => {
+    let searchPhrase = event.target.value;
+    setSearchPhrase(searchPhrase);
+  }
+
+  return (
+    <main className="flex flex-col bg-local justify-center m-2 bg-cover w-72 bg-black w">
+      <h1 className="m-2 text-xl text-white">PokeApp</h1>
+      <form onSubmit={(event => submitSearch(event))}>
+        <input className="p-2 m-2 w-64 bg-white text-black border-2 border-black border-solid"
+>>>>>>> Stashed changes
           required
           placeholder="Enter Pokemon Name Here"
           id="name"
           type="text"
           onChange={(event) => handleSearch(event)}/>
+<<<<<<< Updated upstream
         <input type="submit" value="Submit"/>
+=======
+        <input className="w-24 m-2 p-2 bg-black text-white border-2 border-white border-solid" type="submit" value="Submit"/>
+>>>>>>> Stashed changes
       </form>
-      
+      <searchPokemon searchWord={searchPhrase}/>
     </main>
   );
 }
--- app/search.js
+++ app/search.js
@@ -1,6 +1,7 @@
 "use client";
 
 import {useState, useEffect } from "react";
+<<<<<<< Updated upstream
     
 
 
@@ -9,6 +10,15 @@ const searchPokemon = ({ searchWord }) =>{
   const [pokemon, setPokemon] = useState([]); 
 
   const fetchPokemon = async (searchWord) => {
+=======
+
+const searchPokemon = ({ searchWord }) => {
+
+    const [pokemon, setPokemon] = useState([]); 
+
+  const fetchPokemon = async (searchWord) => {
+
+>>>>>>> Stashed changes
     try {
         const response = await fetch(
             `https://pokeapi.co/api/v2/pokemon/${searchWord}`
@@ -16,7 +26,11 @@ const searchPokemon = ({ searchWord }) =>{
 
         const data = await response.json();
 
+<<<<<<< Updated upstream
         return data || []; //.seachResults
+=======
+        return data || [];
+>>>>>>> Stashed changes
         } 
 
         catch (error) {
@@ -39,9 +53,20 @@ const searchPokemon = ({ searchWord }) =>{
 
 
 return (
+<<<<<<< Updated upstream
     <div>
         <h2>{searchWord}</h2>
         {/* <p>{pokemon.name}</p> */}
+=======
+    <div className='flex flex-wrap m-4'>
+        <h2>{searchWord}</h2>
+        <p>Name: </p>
+        <p>{pokemon.name}</p>
+        <p>Types: </p>
+        <p>{pokemon.types}</p>
+        <p>Abilities: </p>
+        <p>{pokemon.Abilities}</p>
+>>>>>>> Stashed changes
     </div>
 );
 };
